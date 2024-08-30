290e04bac0eee9e12cedc7ff2d94436e457fdd2b
Alpensin <lotrus@yandex.ru>
2024-08-30 22:37:26 +0300

    interpreter

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,7 @@
 package ast
 
+import "github.com/alpensin/monkey-interpreter/token"
+
 type Node interface {
 	TokenLiteral() string
 }
@@ -23,4 +25,31 @@ func (p *Program) TokenLiteral() string {
 		return ""
 	}
 	return p.Statements[0].TokenLiteral()
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {
+
+}
+
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {
+
+}
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
 }
